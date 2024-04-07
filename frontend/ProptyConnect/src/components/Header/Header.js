2767ac609abff7ab22c33d6a981b4a4101c9f475
ucheogbodo-25 <ucheogbodo25@gmail.com>
2024-04-07 09:50:30 -0700

    update images

--- frontend/ProptyConnect/src/components/Header/Header.js
+++ frontend/ProptyConnect/src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../commons/auth";
-import logo from "../../assets/ProptyConnect.png";
+import logo from "../../assets/ProptyConnect.PNG";
 import { NavLink } from "react-router-dom";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
