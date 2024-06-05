c4d237277e18ce6bcf6498a9e2567ba52defb234
UCHECHUKWU CYRIL OGBODO <125726636+ucheogbodo-25@users.noreply.github.com>
2024-06-05 13:47:02 +0100

    Update User.js

--- backend/api/models/User.js
+++ backend/api/models/User.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema(
-    {
-        email: { type: String, required:true, unique:true },
-        username: {type: String, required:true, unique:true},
-        password: {type: String, required:true, unique:true},
-        isAdmin: {type: String, default: false},
-    }
+  {
+    email: { type: String, required: true, unique: true, lowercase: true },
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isAdmin: { type: Boolean, default: false },
+  },
+  { timestamps: true }
 );
 
 module.exports = mongoose.model("User", UserSchema);
