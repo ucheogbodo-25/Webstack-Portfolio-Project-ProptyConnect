2cc627d951c39531c7f2a245667a493727ac0983
UCHECHUKWU CYRIL OGBODO <125726636+ucheogbodo-25@users.noreply.github.com>
2024-06-05 13:45:58 +0100

    Update Agent.js

--- backend/api/models/Agent.js
+++ backend/api/models/Agent.js
@@ -2,18 +2,18 @@ const mongoose = require("mongoose");
 
 const AgentSchema = new mongoose.Schema(
   {
-    firstname: { type: String, required: true, unique: false },
-    surname: { type: String, required: true, unique: false },
+    firstname: { type: String, required: true },
+    surname: { type: String, required: true },
     address: { type: String, required: true, unique: true },
-    city: { type: String, required: true, unique: false },
-    state: { type: String, required: true, unique: false },
-    country: { type: String, required: true, unique: false },
-    PhoneNumber: { type: String, required: true, unique: true },
+    city: { type: String, required: true },
+    state: { type: String, required: true },
+    country: { type: String, required: true },
+    phoneNumber: { type: String, required: true, unique: true },
     linkedin: { type: String, required: true, unique: true },
-    Twitter: { type: String, required: true, unique: true },
-    Email: { type: String, required: true, unique: true },
+    twitter: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true, lowercase: true },
     profilePic: { type: String, default: "" },
-    isAdmin: { type: String, default: false },
+    isAdmin: { type: Boolean, default: false },
   },
   { timestamps: true }
 );
