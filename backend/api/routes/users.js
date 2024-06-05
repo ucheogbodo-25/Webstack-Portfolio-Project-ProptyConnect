a8a25253bdfb85c363598093ae70a6837c678cba
UCHECHUKWU CYRIL OGBODO <125726636+ucheogbodo-25@users.noreply.github.com>
2024-06-05 13:37:24 +0100

    Update users.js

--- backend/api/routes/users.js
+++ backend/api/routes/users.js
@@ -3,22 +3,17 @@ const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const verify = require("../verifyToken");
 
-
-//UPADTE
+//UPDATE
 router.put("/:id", verify, async (req, res) => {
   if (req.user.id === req.params.id || req.user.isAdmin) {
     if (req.body.password) {
-      req.body.password = bcrypt.hash(
-        req.body.password,
-        'my_secret_key'
-      );//.toString();
+      const salt = await bcrypt.genSalt(10);
+      req.body.password = await bcrypt.hash(req.body.password, salt);
     }
     try {
-      const  updatedUser = await User.findByIdAndUpdate(
+      const updatedUser = await User.findByIdAndUpdate(
         req.params.id,
-        {
-          $set:req.body
-        },
+        { $set: req.body },
         { new: true }
       );
       res.status(200).json(updatedUser);
@@ -26,7 +21,7 @@ router.put("/:id", verify, async (req, res) => {
       res.status(500).json(err);
     }
   } else {
-    res.status(403).json("You can only update your account!")
+    res.status(403).json("You can only update your account!");
   }
 });
 
@@ -40,19 +35,19 @@ router.delete("/:id", verify, async (req, res) => {
       res.status(500).json(err);
     }
   } else {
-    res.status(403).json("You can only delete your account!")
+    res.status(403).json("You can only delete your account!");
   }
 });
 
 //GET
 router.get("/find/:id", async (req, res) => {
-    try {
-      const user = await User.findById(req.params.id);
-      const { password, ...info } = user._doc;
-      res.status(200).json(info);
-    } catch (err) {
-      res.status(500).json(err);
-    }
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...info } = user._doc;
+    res.status(200).json(info);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 //GET ALL
@@ -60,53 +55,41 @@ router.get("/", verify, async (req, res) => {
   const query = req.query.new;
   if (req.user.isAdmin) {
     try {
-      const users = query ? await User.find().sort({_id:-1}).limit(5) : await User.find();
+      const users = query
+        ? await User.find().sort({ _id: -1 }).limit(5)
+        : await User.find();
       res.status(200).json(users);
     } catch (err) {
       res.status(500).json(err);
     }
   } else {
-    res.status(403).json("You are not allowed to see all users...")
+    res.status(403).json("You are not allowed to see all users...");
   }
 });
 
 //GET USER STATS
-router.get("/stats", async (req, res) => {
+router.get("/stats", verify, async (req, res) => { // Added verify middleware
   const today = new Date();
-  const lastYear = today.setFullYear(today.setFullYear() - 1);
-
-  const monthsArray = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
+  const lastYear = new Date(today.setFullYear(today.getFullYear() - 1));
 
   try {
     const data = await User.aggregate([
       {
-        $project:{
-          month: {$month: "$createdAt"}
-        }
-      },{
+        $project: {
+          month: { $month: "$createdAt" },
+        },
+      },
+      {
         $group: {
           _id: "$month",
-          total: {$sum:1}
-        }
-      }
+          total: { $sum: 1 },
+        },
+      },
     ]);
-    res.status(200).json(data)
+    res.status(200).json(data);
   } catch (err) {
-    res.status(500).json(err)
+    res.status(500).json(err);
   }
-})
+});
 
 module.exports = router;
